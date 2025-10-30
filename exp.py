1a9f6def91dd0b42d713db6d9435aea3d2ccf166
jo49973477 <jo49973477@naver.com>
2025-10-30 19:08:49 +0900

    trivial4

--- exp.py
+++ exp.py
@@ -6,6 +6,6 @@ from models.configs import get_b16_config_tuning
 model = VisionTransformer(get_b16_config_tuning(moe=True, gshard=True, mla=True))
 print(model)
 
-tensor_input = torch.randn(1, 3, 244, 244)  # Example input tensor
+tensor_input = torch.randn(1, 3, 224, 224)  # Example input tensor
 output = model(tensor_input)
 print(output)
--- models/modeling.py
+++ models/modeling.py
@@ -95,6 +95,7 @@ class Embeddings(nn.Module):
         x = x.transpose(-1, -2)
         x = torch.cat((cls_tokens, x), dim=1)
 
+        
         embeddings = x + self.position_embeddings
         embeddings = self.dropout(embeddings)
         return embeddings
