c0c09cda9cef958c6a33b94f9391aee75e2c2305
jo49973477 <jo49973477@naver.com>
2025-10-30 19:44:01 +0900

    deleted unnecessary complexity

--- models/modeling_ffn.py
+++ models/modeling_ffn.py
@@ -108,12 +108,6 @@ class Gate(nn.Module):
             scores = scores.sigmoid()
         original_scores = scores
         
-        if self.n_groups > 1:
-            scores = scores.view(x.size(0), self.n_groups, -1)
-            group_scores = scores.topk(2, dim=-1)[0].sum(dim=-1)
-            indices = group_scores.topk(self.topk_groups, dim=-1)[1]
-            mask = scores.new_ones(x.size(0), self.n_groups, dtype=bool).scatter_(1, indices, False)
-            scores = scores.masked_fill_(mask.unsqueeze(-1), float("-inf")).flatten(1)
         indices = torch.topk(scores, self.topk, dim=-1)[1]
         weights = original_scores.gather(1, indices)
         if self.score_func == "sigmoid":
