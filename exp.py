63486e5cbbd175b05f4ebffd6a4a73f3861af97b
jo49973477 <jo49973477@naver.com>
2025-10-30 19:54:56 +0900

    test memory too!

--- exp.py
+++ exp.py
@@ -13,12 +13,25 @@ CONFIGS = {
     'ViT-B_16': get_b16_config_tuning(moe=False, gshard=False, mla=False),
 }
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 for key, config in CONFIGS.items():
     model = VisionTransformer(config)
     model.eval()
-    dummy_input = torch.randn(1, 3, 224, 224) # (B, C, H, W)
+    model.to(device)
+    
+    torch.cuda.empty_cache()
+    torch.cuda.reset_peak_memory_stats(device)
+    
+    dummy_input = torch.randn(1, 3, 224, 224).to(device) # (B, C, H, W)
     st_time = time.time()
     output = model(dummy_input)
     end_time = time.time()
+    
+    peak_memory_bytes = torch.cuda.max_memory_allocated(device)
+    peak_memory_mb = peak_memory_bytes / 1024**2 # 바이트(B)를 메가바이트(MB)로 변환
+    
     print(f"Output shape for {key}: {output[0].shape}")  # (B, num_classes)
     print(f"Time taken for {key}: {end_time - st_time} seconds")
+    print(f"Peak memory usage for {key}: {peak_memory_mb} MB")
+    print("-" * 50)
