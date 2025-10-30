03a90c8c4c0bd1f2e788dd034f9d511504fa1932
jo49973477 <jo49973477@naver.com>
2025-10-30 19:34:09 +0900

    more experiment

--- exp.py
+++ exp.py
@@ -1,11 +1,24 @@
 import torch
+import time
 
 from models.modeling import VisionTransformer
 from models.configs import get_b16_config_tuning
 
-model = VisionTransformer(get_b16_config_tuning(moe=True, gshard=True, mla=True))
-print(model)
+CONFIGS = {
+    'ViT-B_16_all': get_b16_config_tuning(moe=True, gshard=True, mla=True),
+    'ViT-B_16_mla': get_b16_config_tuning(moe=False, gshard=False, mla=True),
+    'ViT-B_16_gshard': get_b16_config_tuning(moe=True, gshard=True, mla=False),
+    'ViT-B_16_moe': get_b16_config_tuning(moe=True, gshard=False, mla=False),
+    'ViT-B_16_moe_mla': get_b16_config_tuning(moe=True, gshard=False, mla=True),
+    'ViT-B_16': get_b16_config_tuning(moe=False, gshard=False, mla=False),
+}
 
-tensor_input = torch.randn(1, 3, 224, 224)  # Example input tensor
-output = model(tensor_input)
-print(output)
+for key, config in CONFIGS.items():
+    model = VisionTransformer(config)
+    model.eval()
+    dummy_input = torch.randn(2, 197, config.hidden_size)  # (B, S, D)
+    st_time = time.time()
+    output = model(dummy_input)
+    end_time = time.time()
+    print(f"Output shape for {key}: {output[0].shape}")  # (B, num_classes)
+    print(f"Time taken for {key}: {end_time - st_time} seconds")
