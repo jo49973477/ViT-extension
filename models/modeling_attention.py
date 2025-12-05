f49df7939ce6508e2c6faf58adaa681f85134a0a
jo49973477 <jo49973477@naver.com>
2025-12-06 00:43:25 +0900

    trivial

--- models/modeling_attention.py
+++ models/modeling_attention.py
@@ -315,7 +315,7 @@ class MultiHeadLatentAttentionViTWithROPE(nn.Module):
         Returns:
             torch.Tensor: Output tensor with the same shape as the input.
         """
-        freqs_cis = self.precompute_freqs_cis()
+        freqs_cis = self.precompute_freqs_cis().to(torch.device("cuda"))
 
         bsz, seqlen, _ = x.size()
 
